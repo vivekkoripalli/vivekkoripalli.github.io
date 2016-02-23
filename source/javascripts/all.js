7d114b68546e2f0f1d00e7ce4b80af1c3fba9a72
Melchior Rutte <melchior.rutte@gmail.com>
2016-02-23 18:02:43 +0100

    portfolio in progress

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,14 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+$(document).ready(function() {
+  $('.tab').on('click', function(event) {
+    $('.tab').removeClass('active');
+    $(this).addClass('active');
+
+    $(".tab-content").addClass('hidden');
+    tabSelector = $(this).data("target");
+    $(tabSelector).removeClass("hidden");
+
+  });
+});
