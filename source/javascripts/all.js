e36bf8f31cbefc66e4feab125adfb4b081e3129b
Melchior Rutte <melchior.rutte@gmail.com>
2016-02-25 14:21:03 +0100

    porfolio in progress

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,14 +1,33 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
-$(document).ready(function() {
-  $('.tab').on('click', function(event) {
-    $('.tab').removeClass('active');
-    $(this).addClass('active');
 
-    $(".tab-content").addClass('hidden');
-    tabSelector = $(this).data("target");
-    $(tabSelector).removeClass("hidden");
 
+jQuery(document).ready(function($){
+  // browser window scroll (in pixels) after which the "back to top" link is shown
+  var offset = 300,
+    //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+    offset_opacity = 1200,
+    //duration of the top scrolling animation (in ms)
+    scroll_top_duration = 700,
+    //grab the "back to top" link
+    $back_to_top = $('.cd-top');
+
+  //hide or show the "back to top" link
+  $(window).scroll(function(){
+    ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+    if( $(this).scrollTop() > offset_opacity ) {
+      $back_to_top.addClass('cd-fade-out');
+    }
   });
+
+  //smooth scroll to top
+  $back_to_top.on('click', function(event){
+    event.preventDefault();
+    $('body,html').animate({
+      scrollTop: 0 ,
+      }, scroll_top_duration
+    );
+  });
+
 });
